fe6b175ccd0d5d8009baef0f0bf656d0a44ce948
CyrilRM <cyril.rolland.monnet2e1@gmail.com>
2020-03-19 15:43:38 +0100

    Ajout d'un form ville

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
+router.post('/ville', function(req, res, next) {
+  res.render('ville', { ville: req.body.ville });
+});
 
 module.exports = router;
