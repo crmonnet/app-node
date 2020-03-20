5c006099b533b82270bb4f81f41f5ac6d924e793
CyrilRM <cyril.rolland.monnet2e1@gmail.com>
2020-03-20 11:58:52 +0100

    geo loc

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require('axios')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
-router.post('/ville', function(req, res, next) {
-  res.render('ville', { ville: req.body.ville });
-});
+
+router.post('/ville', async function(req, res, next) {
+  var param = req.body.ville
+  param = param.replace(" ", "+")
+  console.log(param)
+  var url = "https://geocode.xyz/" + param + "?json=1"
+  var jsonData
+  try {
+    await axios.get(url).then(response => {
+      jsonData = response.data
+    })
+    console.log(jsonData)
+    if (jsonData['error'] != null) {
+      res.render('error', {
+        message: jsonData['error']['description']
+      })
+    }
+    else {
+      res.render('ville', {
+        ville: req.body.ville,
+        longitude: jsonData["longt"],
+        latitude: jsonData["latt"]})
+      }
+  }
+  catch (err) {
+    res.send(err)
+  }
+
+})
 
 module.exports = router;
